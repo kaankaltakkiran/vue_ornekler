f52ec3ec488f32a6a5318202e209a98089428038
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-21 13:40:39 +0300

    learn key events

--- key_event/app.js
+++ key_event/app.js
@@ -5,6 +5,19 @@
    data diye  metot oluşturup return ile obje dönüyoruz */
   /*  değişken,obje,dizi tanımlama */
   createApp({
- 
+   data(){
+    return{
+      title:"Klavye Eventleri",
+    }
+   },
+    methods:{
+      // metotlar
+       isimYaz:function(){
+        console.log("İsim alanına yazıldı");
+      },
+      yasYaz:function(){
+        console.log("Yaş alanına yazıldı");
+      },
+    }
   }).mount('#app');
  
