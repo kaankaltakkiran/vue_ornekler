094269e953daabef37c305ab620b329a357c9472
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-21 15:25:55 +0300

    learn dinamik css

---
+++ dinamik_css/app.js
@@ -0,0 +1,15 @@
+  // Vudeki CreateApp fonksiyonunu kullanarak bir uygulama oluşturuyoruz
+  const { createApp } = Vue;
+  /*  {} içine parametleler için
+   mount ile nereye monte edileceğini belirtiyoruz
+   data diye  metot oluşturup return ile obje dönüyoruz */
+  /*  değişken,obje,dizi tanımlama */
+  createApp({
+data(){
+  return{
+    title:"Dinamik Css",
+    isActive:false
+  }
+}
+  }).mount('#app');
+ 
