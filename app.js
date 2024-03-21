11e554d0ba0164b182fd92e176856a8e7f70c3ac
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-21 13:27:45 +0300

    event modifires

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
   // Vudeki CreateApp fonksiyonunu kullanarak bir uygulama oluşturuyoruz
-const { createApp } = Vue;
- /*  {} içine parametleler için
-  mount ile nereye monte edileceğini belirtiyoruz
-  data diye  metot oluşturup return ile obje dönüyoruz */
- /*  değişken,obje,dizi tanımlama */
- createApp({
+  const { createApp } = Vue;
+  /*  {} içine parametleler için
+   mount ile nereye monte edileceğini belirtiyoruz
+   data diye  metot oluşturup return ile obje dönüyoruz */
+  /*  değişken,obje,dizi tanımlama */
+  createApp({
+   
+  }).mount('#app');
  
- }).mount('#app');
