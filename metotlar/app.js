58ed7ee2077e9b8044be52c51c2ba53820723656
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-21 12:12:15 +0300

    learn methots

--- metotlar/app.js
+++ metotlar/app.js
@@ -5,5 +5,20 @@ const { createApp } = Vue;
   data diye  metot oluşturup return ile obje dönüyoruz */
  /*  değişken,obje,dizi tanımlama */
  createApp({
-
+  data(){
+    return{
+     age:25,
+     a:10,
+    b:20,
+    }
+  },
+  methods:{
+    // metotlar
+    selamla:function(name,surname){
+      return "Merhaba "+name+" "+surname+" "+this.age+" yaşındasın";
+    },
+    toplama:function(){
+      return "Toplam: "+(this.a+this.b);
+    },
+  }
  }).mount('#app');
