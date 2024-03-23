2ffd94c48700a15ced4a0f9b29c7076d355d9dad
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-23 19:23:52 +0300

    example login form with style slot

--- vue-project2/src/App.vue
+++ vue-project2/src/App.vue
@@ -2,12 +2,29 @@
 <alt-component>
  <!--  gönderilen html -->
 <!--  isimlendirmiş slot -->
- <template #Title>
-  <h2>Alt Compenent Title</h2>
-</template>
-<template #Paragraph>
-  <p>Alt Component Paragraph</p>
-</template>
+<template #form-header>
+      <h2>Giriş Formu</h2>
+      <p>Giriş yapabilmek için lütfen formu doldurun</p>
+      <p v-bind:class="$style.blue">örnek stlye module kullanımı</p>
+      <p v-bind:class="xbilesen.green">örnek stlye module kullanımı</p>
+    </template>
+
+    <template #form-elements>
+      <label for="email">E-Posta</label>
+      <input type="email" name="email" placeholder="E-posta adresinizi giriniz">
+
+      <label for="password">Şifre</label>
+      <input type="password" name="password" placeholder="Şifrenizi giriniz">
+    </template>
+
+    <template #form-controls>
+      <button>Giriş Yap</button>
+    </template>
+
+    <template>
+      <p>Tüm Hakları Saklıdır. kaan baa</p>
+    </template>
+
 </alt-component>
 </template>
 
@@ -29,4 +46,14 @@ export default{
 </script>
 
 <style scoped>
+:global(h2){color:red}
+:deep(input){width:100%; border:1px solid #e2e2e2; height:35px; border-radius: 5px; margin:5px 0 15px 0; padding-left:15px}
+</style>
+
+<style module>
+.blue{color: blue;}
+</style>
+
+<style module="xbilesen">
+.green{color: green;}
 </style>
