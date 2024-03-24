e5422db28610a34eedc74b18f0b73700c8a3f399
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-24 15:55:23 +0300

    createWebHistory vs createWebHashHistory

--- vue-input/src/routes.js
+++ vue-input/src/routes.js
@@ -1,11 +1,12 @@
 /* history yönetemi tek sayfa için uygun */
-import { createWebHashHistory, createRouter } from "vue-router";
+//!createWebHashHistory
+import { createWebHistory, createRouter } from "vue-router";
 //componenet import
 import ShowBlogs from "./components/ShowBlogs.vue";
 import AddBlog from "./components/AddBlog.vue";
 
 const router= createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: [
         { path: "/", component: ShowBlogs },
         { path: "/add", component: AddBlog }
