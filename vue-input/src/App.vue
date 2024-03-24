3c66de90c2c4f71a358e13a4ad3b9fa207c641f0
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-24 11:09:18 +0300

    learn fetch get method

--- vue-input/src/App.vue
+++ vue-input/src/App.vue
@@ -1,12 +1,15 @@
 <template>
-<add-blog></add-blog>
+<!-- <add-blog></add-blog> -->
+<show-blogs></show-blogs>
 </template>
 
 <script>
 import AddBlog from './components/AddBlog.vue'
+import ShowBlogs from './components/ShowBlogs.vue'
 export default {
   components: {
-   "add-blog":AddBlog
+   "add-blog":AddBlog,
+    "show-blogs":ShowBlogs
   },
   data() {
     return {
---
+++ vue-input/src/components/ShowBlogs.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="show-blogs">
+    <h1>Blogs</h1>
+    <div class="single-blog" v-for="blog in blogs">
+    <h2>Title:{{ blog.title }} </h2>
+    <p>Content: {{ blog.body }} </p>
+  </div>
+  </div>
+</template>
+<script>
+export default{
+  data(){
+    return{
+      blogs:[]
+  }
+  },
+ /*  instance oluştuğunda çalışır */
+  created(){
+    // instance bilgilerini al 
+    var self = this;
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+
+method: "GET",
+/*     auth veya  karakter tanımlamak için kullanılır */
+headers: {
+    "Content-Type": "content/type"
+},
+}).then((res) => res.json())
+.then((function (json) {
+console.log(json);
+/* 10 tanesini al * slice ile */
+self.blogs = json.slice(0, 10);
+}))
+.catch((function(err){
+console.log(err);
+}));
+},
+}
+</script>
+
+<style scoped>
+#show-blogs{
+    max-width: 800px;
+    margin: 0px auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background-color: #e2e2e2;
+    border-radius: 5px;
+}
+</style>
