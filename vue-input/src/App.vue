a7c425bee51be5f5dfefaeae1939e4e59743dc6c
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-24 09:58:47 +0300

    learn input,checkbox and select html

---
+++ vue-input/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<add-blog></add-blog>
+</template>
+
+<script>
+import AddBlog from './components/AddBlog.vue'
+export default {
+  components: {
+   "add-blog":AddBlog
+  },
+  data() {
+    return {
+      message: 'Hello Vue!',
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
