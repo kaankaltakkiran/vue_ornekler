861ec905cf0fb7663e99749867a55cbecdf7e902
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-03-24 16:03:45 +0300

    learn router link

--- vue-input/src/App.vue
+++ vue-input/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <app-header></app-header>
   <!--   Tanımlı routeları görüntülemek -->
     <router-view>
     </router-view>
@@ -10,11 +11,13 @@
 import AddBlog from './components/AddBlog.vue'
 import ShowBlogs from './components/ShowBlogs.vue'
 import ListBlogs from './components/ListBlogs.vue'
+import Header from './components/Header.vue' 
 export default {
   components: {
    "add-blog":AddBlog,
     "show-blogs":ShowBlogs,
-    "list-blogs":ListBlogs
+    "list-blogs":ListBlogs,
+    "app-header":Header
   },
   data() {
     return {
---
+++ vue-input/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+      <ul>
+<!--          router-link componenti ile sayfalar arasında geçiş yapabiliyoruz to ile hangi sayfaya geçeceğimizi belirtiyoruz -->
+          <li><router-link to="/">Blog'ları Göster</router-link></li>
+          <li><router-link to="/add">Blog Ekle</router-link></li>
+      </ul>
+  </nav>
+</template>
+
+<style scoped>
+ul{
+  list-style-type: none;
+  text-align: center;
+  margin: 0;
+}
+li{
+  display: inline-block;
+  margin: 0 10px;
+}
+a{
+  color: #fff;
+  text-decoration: none;
+  padding: 6px 8px;
+  border-radius: 10px;
+}
+nav{
+  background: #444;
+  padding: 14px 0;
+  margin-bottom: 40px;
+}
+.router-link-active{
+  background: #eee;
+  color: #444;
+}
+</style>
